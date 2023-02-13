b7dd6ec20185923835b30c172a64aff99c4b8f04
Giovanni Capelli <giovacape01@gmail.com>
2023-02-13 03:32:01 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import AppMain from './components/AppMain.vue';
+export default {
+  components: {
+    AppMain,
+  }
+}
+</script>
+
+<template>
+  <AppMain />
+</template>
+
+<style lang="scss">
+@use './assets/styles/generals.scss' as *
+</style>
---
+++ src/store.js
@@ -0,0 +1,12 @@
+import { reactive } from 'vue';
+
+export const store = reactive({
+    cardsList: [],
+    apiKey: '58f82a81001bd35dae6840f503464d60',
+    apiFilm: 'https://api.themoviedb.org/3/movie/550?api_key=58f82a81001bd35dae6840f503464d60&language=en-US',
+    apiTv: 'https://api.themoviedb.org/3/search/tv?api_key=58f82a81001bd35dae6840f503464d60&language=en-US'
+
+
+})
+
+// 58f82a81001bd35dae6840f503464d60 QUESTA é LA MIA KEY!!
